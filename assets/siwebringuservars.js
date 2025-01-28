209e0e569ccdfb88a95bbc4b4f5fa58c72c8a209
Miruki <milkytrackz@gmail.com>
2025-01-28 13:11:54 +0800

    added the widget for the self-insert webring :3

---
+++ assets/siwebringuservars.js
@@ -0,0 +1,6 @@
+//only edit these top variables! do not edit the last line where the variable is "text" or else your webring won't display what you've set properly!
+var name = "Lumi";
+var thing = "Wonderhoying";
+var character = "Emu Otori";
+var media = "Project Sekai: Colorful Stage!";
+var image = "https://milkyway.moe/emuotori.png"
