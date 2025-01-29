339cbdb101cbde50d683ce1bae7798ed8608f294
MiLKY <webmaster@milkyway.moe>
2025-01-29 10:58:34 +0800

    Revert "Seperated the sites from the onionring variables. God help."
    This reverts commit 6362a6fa3293c7ad337b3539f00cb15d3e52491f.

--- onlinewire/onionring-variables.js
+++ onlinewire/onionring-variables.js
@@ -1,25 +1,33 @@
+// onionring.js is made up of four files - onionring-widget.js, onionring-index.js, onionring-variables.js (this one!), and onionring.css
+// it's licensed under the cooperative non-violent license (CNPL) v4+ (https://thufie.lain.haus/NPL.html)
+// it was originally made by joey + mord of allium (蒜) house, last updated 2020-11-24
+
 // === ONIONRING-VARIABLES ===
-// This file contains the stuff you edit to set up your specific webring
+//this file contains the stuff you edit to set up your specific webring
 
-// Fetch sites from an external file
-var sites = [];
+//the full URLs of all the sites in the ring
+var sites = [
+'https://milkyway.moe/',
+'https://ddnikki.nekoweb.org/',
+'https://rhzartist.neocities.org/',
+'https://sonnenboy.eu/',
+'https://pastel-skies.nekoweb.org',
+'https://astracelestine.nekoweb.org/',
+'https://harlequi.nz/'
+];
 
-fetch('sites.txt')
-  .then(response => response.text())
-  .then(text => {
-    sites = text.split('\n').map(site => site.trim()).filter(site => site.length > 0);
-  })
-  .catch(error => console.error('Error loading sites:', error));
-
-// The name of the ring
+//the name of the ring
 var ringName = 'OnlineWire';
 
-// The unique ID of the widget
+/* the unique ID of the widget. two things to note:
+ 1) make sure there are no spaces in it - use dashes or underscores if you must
+ 2) remember to change 'webringid' in the widget code you give out and all instances of '#webringid' in the css file to match this value!*/
 var ringID = 'onlinewire-webring';
 
-// Should the widget include a link to an index page?
+//should the widget include a link to an index page?
 var useIndex = true;
+//the full URL of the index page. if you're not using one, you don't have to specify anything here
 var indexPage = 'https://milkyway.moe/onlinewire/index.html';
 
-// Should the widget include a random button?
+//should the widget include a random button?
 var useRandom = true;
